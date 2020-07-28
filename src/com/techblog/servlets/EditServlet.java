ef3e3e818fcc0e167a6d37a0f4b5a8cbbee6452f
ashish <ashishtomar692@gmail.com>
2020-07-28 14:00:59 +0530

    commit

--- src/com/techblog/servlets/EditServlet.java
+++ src/com/techblog/servlets/EditServlet.java
@@ -23,7 +23,7 @@ import com.techblog.helper.Helper;
 @MultipartConfig
 public class EditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    	private static final String SAVE_DIR="img";//This is our folder name
+    	private static final String SAVE_DIR="pics";//This is our folder name
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
